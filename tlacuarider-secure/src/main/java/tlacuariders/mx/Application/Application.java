e38cfb5fd1f505bf19beeb67ef6ac0f341f6f91e
Cesar <cesare_ar@hotmail.com>
2021-08-05 01:46:41 -0500

    Correccion de aplicación

--- tlacuarider-secure/src/main/java/tlacuariders/mx/Application/Application.java
+++ tlacuarider-secure/src/main/java/tlacuariders/mx/Application/Application.java
@@ -1,4 +1,4 @@
-package tlacuariders.mx;
+package tlacuariders.mx.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
